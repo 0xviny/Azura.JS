2c1d5c6884067444f54180ec400235bc4c48405a
Wyntch <122948478+Thinxy@users.noreply.github.com>
2025-04-28 21:03:58 -0300

    feat: adicionando a cli na package.

--- src/core/Server.ts
+++ src/core/Server.ts
@@ -1,4 +1,4 @@
-import http from "http";
+import http, { IncomingMessage, ServerResponse } from "http";
 import cluster from "cluster";
 import os from "os";
 import { Router } from "./Router";
@@ -12,6 +12,16 @@ export interface ServerOptions {
   https?: boolean;
   http2?: boolean;
   cluster?: boolean;
+}
+
+type Handler = (ctx: any) => Promise<void> | void;
+export type RequestHandler = (req: IncomingMessage, res: ServerResponse) => Promise<void>;
+
+function adaptRequestHandler(handler: RequestHandler): Handler {
+  return async (ctx: any) => {
+    const { request, response } = ctx;
+    await handler(request, response);
+  };
 }
 
 export class Server {
@@ -31,6 +41,7 @@ export class Server {
     this.server.on("request", this.handle.bind(this));
   }
 
+  // Método para carregar controladores
   public load(controllers: any[]): void {
     applyDecorators(this, controllers);
   }
@@ -48,12 +59,28 @@ export class Server {
     this.plugins.register(plugin, opts);
   }
 
-  addRoute(method: string, path: string, handler: any) {
-    this.router.add(method, path, handler);
+  addRoute(method: string, path: string, handler: RequestHandler) {
+    this.router.add(method, path, adaptRequestHandler(handler));
   }
 
   onHook(type: any, fn: Function) {
     this.life.add(type, fn);
+  }
+
+  public get(path: string, handler: RequestHandler): void {
+    this.addRoute("GET", path, handler);
+  }
+
+  public post(path: string, handler: RequestHandler): void {
+    this.addRoute("POST", path, handler);
+  }
+
+  public put(path: string, handler: RequestHandler): void {
+    this.addRoute("PUT", path, handler);
+  }
+
+  public delete(path: string, handler: RequestHandler): void {
+    this.addRoute("DELETE", path, handler);
   }
 
   listen(port = this.opts.port || 3000) {
@@ -64,17 +91,17 @@ export class Server {
     }
   }
 
-  private async handle(req: any, res: any) {
+  private async handle(req: IncomingMessage, res: ServerResponse) {
     const id = uuid();
     const ctx: any = {
       id,
       request: req,
       response: res,
       params: {},
-      query: Object.fromEntries(new URLSearchParams(req.url.split("?")[1] || "")),
+      query: Object.fromEntries(new URLSearchParams(req.url?.split("?")[1] || "")),
     };
     try {
-      const handler = this.router.find(req.method, req.url.split("?")[0]);
+      const handler = this.router.find(req.method!, req.url?.split("?")[0] || "");
       await handler(ctx);
     } catch (e: any) {
       if (e instanceof HTTPError) {
